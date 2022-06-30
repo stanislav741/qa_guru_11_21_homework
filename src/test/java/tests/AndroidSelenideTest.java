db8cc25ff71ed3a106523afda32a6ef152381065
stanislav741 <stasdmitruk1@gmail.com>
2022-06-30 16:27:45 +0200

    Updated the test scenario

--- src/test/java/tests/AndroidSelenideTest.java
+++ src/test/java/tests/AndroidSelenideTest.java
@@ -1,5 +1,32 @@
 package tests;
 
-public class AndroidSelenideTest extends TestBase {
+import io.appium.java_client.MobileBy;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static io.qameta.allure.Allure.step;
+
+public class AndroidSelenideTest extends TestBase {
+    @Owner("stanislav741")
+    @DisplayName("Homework - Wiki Andrid App - Search test")
+    @Tag("selenide")
+    @Test
+    void searchSmokeTest() {
+        step("Skip the welcome screen", () ->
+                $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click());
+
+        step("Use the search field", () -> {
+            $(MobileBy.AccessibilityId(("Search Wikipedia"))).click();
+            $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("BrowserStack");
+        });
+
+        step("Verify the returned list is not empty", () ->
+                $$(byClassName("android.widget.TextView")).shouldHave(sizeGreaterThan(0)));
+    }
 }
